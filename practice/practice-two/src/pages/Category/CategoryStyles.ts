8ffa37f3f1d87e41149ee041c9df7cbfe999e730
Luan Vu <luan.vu@asnet.com.vn>
2025-05-28 10:13:13 +0700

    Update CategoryStyles.ts

--- practice/practice-two/src/pages/Category/CategoryStyles.ts
+++ practice/practice-two/src/pages/Category/CategoryStyles.ts
@@ -7,6 +7,7 @@ import styled from 'styled-components'
 export const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
+  padding: 0;
 `
 
 export const PageHeader = styled.div`
